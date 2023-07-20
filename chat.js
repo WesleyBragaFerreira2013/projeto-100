a4952c1473b1f48166b7d3f641232b4dbc7457ae
WesleyBragaFerreira2013 <129886422+WesleyBragaFerreira2013@users.noreply.github.com>
2023-07-20 19:49:14 -0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -14,8 +14,8 @@ firebase.initializeApp(firebaseConfig);*/
 
 function addUser()
 {
-    /*user_name = document.getElementById("user_name").value;
-    firebase.database().ref("/").child(user_name).update({
+    user_name = document.getElementById("user_name").value;
+    /*firebase.database().ref("/").child(user_name).update({
         purpose : "adding user"
     });*/
     /*
